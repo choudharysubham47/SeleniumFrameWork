e72a481cd4e659651c9f43547dcf30395c7bdf1d
choudharysubham47 <choudharysubham47@gmail.com>
2022-02-07 13:11:50 +0530

    add launch code for chrome

--- src/test/java/LaunchChrome.java
+++ src/test/java/LaunchChrome.java
@@ -1,7 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LaunchChrome {
 
     public static void main(String s[])
+
     {
+
+      // String path = System.getProperty("user.dir");
         System.out.println("We Are Ready To launch");
+        //System.setProperty("webdriver.chrome.driver",path+"//chromedriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/Users/admin/Desktop/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+       // driver.manage().deleteAllCookies();
+        driver.get("https://www.selenium.dev/downloads/");
+
+        driver.close();
     }
 }
+
+
+/*Note:
+======
+If chromeDriver is throwing error that it cannot run on mac
+then goto teminal and navigate to chromedriver path
+and run this command
+
+        xattr -d com.apple.quarantine chromedriver */
